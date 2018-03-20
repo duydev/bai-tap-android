82a5cd62e41ae739fc8136a6779223589503dfe3
Trần Nhật Duy <trannhatduy297@gmail.com>
2018-03-21 00:25:19 +0700

    Done BaiTap Bai 4

--- BaiTapBai4/app/src/main/java/me/duydev/baitapbai4/BaiTap3.java
+++ BaiTapBai4/app/src/main/java/me/duydev/baitapbai4/BaiTap3.java
@@ -2,6 +2,10 @@ package me.duydev.baitapbai4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class BaiTap3 extends AppCompatActivity {
 
@@ -10,4 +14,27 @@ public class BaiTap3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bai_tap3);
     }
+
+    public void setColor(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView2);
+
+        switch (view.getId()) {
+            case R.id.btnGreen:
+                textView.setTextColor(getResources().getColor(R.color.green));
+                break;
+            case R.id.btnBlue:
+                textView.setTextColor(getResources().getColor(R.color.blue));
+                break;
+            default:
+                // case red
+                textView.setTextColor(getResources().getColor(R.color.red));
+
+        }
+    }
+
+    public void clearColor(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setTextColor(getResources().getColor(R.color.black));
+    }
+
 }
